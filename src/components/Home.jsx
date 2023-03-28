95d21adde9a32cbf63f8942e8442505a4bc10311
mailhyuil <mailhyuil@gmail.com>
2023-03-28 22:25:14 +0900

    chore:update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,9 +13,8 @@ const Home = () => {
         initial={{ x: -100, y: -100 }}
         animate={{ color: "rgb(250,200,0)", x: 0, y: 0 }}
         transition={{ delay: 0.2, type: "spring", stiffness: 500 }}>
-        <h1 className="font-black lg:text-8xl text-4xl text-gray-800 ">
-          Welcome to My Blog!
-        </h1>
+        <h1 className="font-black lg:text-8xl text-4xl text-gray-800 ">Welcome to My Blog!</h1>
+
         <h1 className="font-black lg:text-6xl text-2xl text-gray-800 ">
           My Name is&ensp;
           <span className="before:block before:absolute before:-inset-1 before:-skew-y-6 before:bg-pink-500/80 p-2 relative inline-block">
