a71add8a8cc0779601f31e5f18d81fdc402fbf06
mailhyuil <mailhyuil@gmail.com>
2024-06-29 18:15:44 +0900

    chore:update blog

--- src/Index.jsx
+++ src/Index.jsx
@@ -8,8 +8,14 @@ const Main = () => {
   const [foundBlog, setFoundBlog] = useState([]);
   const [query, setQuery] = useState("");
   const [navIndex, setNavIndex] = useState("home");
-  const blog_category = useRef([]);
-  const nav_ul = useRef();
+  const blogCategory = useRef([]);
+  const categories = new Set();
+  const navUl = useRef();
+  const menuList = {
+    home: <Home />,
+    blog: blog,
+  };
+
   useEffect(() => {
     const res = getAllMDFile().filter((md) => {
       const splitQuery = query.split(" ");
@@ -27,11 +33,13 @@ const Main = () => {
     }
   }, [query]);
 
-  const categories = new Set();
-
   const getAllMDFile = () => {
     return require.context("/public/posts", true, /\.md$/).keys();
   };
+
+  getAllMDFile()
+    .map((text) => text.split("/")[1])
+    .map((e) => categories.add(e));
 
   const getMDFilesByCategory = (cat) => {
     const list = [];
@@ -43,21 +51,12 @@ const Main = () => {
     return list;
   };
 
-  getAllMDFile()
-    .map((text) => text.split("/")[1])
-    .map((e) => categories.add(e));
-
   const onChange = (e) => {
     setQuery(e.target.value);
   };
 
-  const menuList = {
-    home: <Home />,
-    blog: blog,
-  };
-
   const onClickBlog = (event) => {
-    if (event.target.classList.contains("blog")) blog_category.current.map((e) => e.classList.toggle("hidden"));
+    if (event.target.classList.contains("blog")) blogCategory.current.map((e) => e.classList.toggle("hidden"));
   };
 
   const onClickBlogCategory = (event, e) => {
@@ -72,7 +71,7 @@ const Main = () => {
       <div className="flex flex-col flex-1 h-full bg-white lg:flex-row">
         <nav className="overflow-y-auto lg:h-full scrollbar-hide font-primary">
           <div className="flex justify-center p-3">
-            <img width="128" height="108" className="mt-5 hidden lg:block" src="/img/myblog_logo.webp" alt="logo" />
+            <img width="128" height="108" className="hidden mt-5 lg:block" src="/img/myblog_logo.webp" alt="logo" />
             <img width="96" height="41" className="lg:hidden" src="/img/myblog_logo2.webp" alt="logo" />
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -82,29 +81,29 @@ const Main = () => {
               stroke="currentColor"
               className="absolute w-12 h-12 lg:hidden left-3 top-1"
               onClick={() => {
-                nav_ul.current.classList.toggle("hidden");
+                navUl.current.classList.toggle("hidden");
               }}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
           </div>
-          <ul className="flex-col items-start hidden px-4 cursor-pointer lg:flex" ref={nav_ul}>
+          <ul className="flex-col items-start hidden px-4 cursor-pointer lg:flex" ref={navUl}>
             <motion.li
               initial={{ x: -400 }}
               animate={{ x: 0 }}
-              className="px-4 w-full py-3 text-2xl font-black text-gray-800 hover:text-pink-500"
+              className="w-full px-4 py-3 text-2xl font-black text-gray-800 hover:text-pink-500"
               onClick={() => {
                 setNavIndex("home");
-                nav_ul.current.classList.toggle("hidden");
+                navUl.current.classList.toggle("hidden");
               }}>
               Home
             </motion.li>
 
             <motion.li
-              className="px-4 w-full py-3 text-2xl font-black text-gray-800 blog hover:text-pink-500"
+              className="w-full px-4 py-3 text-2xl font-black text-gray-800 blog hover:text-pink-500"
               initial={{ x: -400 }}
               animate={{ x: 0 }}
               onClick={(e) => {
-                onClickBlog(e, blog_category);
+                onClickBlog(e, blogCategory);
               }}>
               Blog
               {Array.from(categories).map((e, index) => {
@@ -112,7 +111,7 @@ const Main = () => {
                   <ul
                     className="hidden text-xl font-semibold text-gray-800 transition-all ease-in border-b border-dashed blog-category border-slate-400 hover:text-pink-500"
                     key={index}
-                    ref={(el) => (blog_category.current[index] = el)}
+                    ref={(el) => (blogCategory.current[index] = el)}
                     onClick={(event) => {
                       onClickBlogCategory(event, e);
                     }}>
@@ -125,7 +124,7 @@ const Main = () => {
                           onClick={() => {
                             setNavIndex("blog");
                             setBlog(<Blog fileName={md} />);
-                            nav_ul.current.classList.toggle("hidden");
+                            navUl.current.classList.toggle("hidden");
                             setFoundBlog([]);
                             setQuery("");
                           }}>
@@ -139,15 +138,9 @@ const Main = () => {
             </motion.li>
           </ul>
         </nav>
-        <motion.main className="flex-1 flex flex-col overflow-y-auto scrollbar-hide">
+        <motion.main className="flex flex-col flex-1 overflow-y-auto scrollbar-hide">
           <div className="p-2">
-            <input
-              autoFocus
-              placeholder="Search whatever you want to search!"
-              className="border-2 rounded-md py-1 px-2 font-bold w-full "
-              value={query}
-              onChange={onChange}
-            />
+            <input autoFocus placeholder="Search whatever you want to search!" className="w-full px-2 py-1 font-bold border-2 rounded-md " value={query} onChange={onChange} />
           </div>
           <div className="flex-1">
             <ul>
@@ -158,10 +151,10 @@ const Main = () => {
                     onClick={() => {
                       setNavIndex("blog");
                       setBlog(<Blog fileName={blog.split("/")[2].split(".")[0]} />);
-                      nav_ul.current.classList.toggle("hidden");
+                      navUl.current.classList.toggle("hidden");
                       setFoundBlog([]);
                     }}
-                    className="cursor-pointer hover:bg-gray-100 border-b p-1 font-semibold text-sm text-gray-600">
+                    className="p-1 text-sm font-semibold text-gray-600 border-b cursor-pointer hover:bg-gray-100">
                     {blog}
                   </li>
                 );
